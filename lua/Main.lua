30e07dbcb3cd7568333427a484c4bb79f4648a87
RaphaelIT7 <raphael.i.t@outlook.de>
2023-01-17 19:39:43 +0100

    [Lua] oops

--- lua/Main.lua
+++ lua/Main.lua
@@ -1,6 +1,5 @@
-print("Hello World!", _G)
-print(discord.create("MTA2MzYxNjE4Mzk1OTU2MDIzMg.GmOi1a.L2FhoVTRF6uIuuvglqFR5DIyIwHrwCO6qwliLs"))
+discord.create("[Token]")
 
-discord.registercommand("test2", "a", function(tbl)
-	print("LOL", tbl.getMember().isOnlineStatus("ONLINE"))
+discord.registercommand("test", "a", function(tbl)
+	print("WORKED")
 end)
